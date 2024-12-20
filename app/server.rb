26f0f1b2ad2ab69139859cdf4e62049390f3a5be
Wilson <wilsontung168@gmail.com>
2024-12-19 18:30:37 -0800

    added file uploads to database

--- app/server.rb
+++ app/server.rb
@@ -3,7 +3,13 @@ require 'securerandom'
 require 'sqlite3'
 require 'json'
 
-# Open storage.db
+############### Project Directories and Paths ###############
+
+#/
+#  DATABASE 
+#/
+
+# Database file path
 DB_FILE = File.expand_path('../storage.db', __dir__)
 
 # If storage.db does not exist, create a new storage.db by calling db_setup.rb
@@ -20,6 +26,23 @@ DB.results_as_hash = true
 before do
     content_type :json
 end
+
+#/
+#  UPLOADS 
+#/
+
+# Uploads folder path
+UPLOADS_DIR = File.expand_path('../uploads', __dir__)
+
+# Ensure uploads folder exists
+Dir.mkdir(UPLOADS_DIR) unless Dir.exist? UPLOADS_DIR
+
+
+################ HTTP REQUESTS AND RESPONSES ###############
+
+#/
+#  OBJECTS
+#/
 
 # Upload Object
 post '/object' do
@@ -39,7 +62,7 @@ end
 # Fetch object
 get '/object/:id' do
     id = params['id']
-    result = DB.execute("SELECT content FROM objects WHERE id = ?", id).first
+    result = DB.execute("SELECT * FROM objects WHERE id = ?", id).first
     halt 404, { message: 'Object not found' }.to_json unless result
     { 
         id: id,
@@ -78,3 +101,58 @@ get '/objects' do
         end.to_json
     end
 end
+
+#/
+#  UPLOADS
+#/
+
+# Upload FILE
+post '/upload' do
+    id = params['id'] || SecureRandom.uuid
+
+    # Get sinatra's file object: {filename: "example.pdf", type: # MIME type of the file, tempfile: # temporary file location}
+    file = params['file']
+    halt 404, { error: 'No file provided' }.to_json unless file
+
+    # Write the file path to uploads folder with file_name: uuid of the file
+    file_path = File.join(UPLOADS_DIR, id)
+    File.open(file_path, 'wb') { |f| f.write(file[:tempfile].read)} # Reads the file from temp location and writes it to uploads folder
+
+    uploaded_at = Time.now.strftime('%Y-%m-%d %H:%M:%S')
+    # Insert file metadata into the database
+    DB.execute('INSERT INTO uploads (id, file_name, file_path, content_type, uploaded_at) VALUES (?, ?, ?, ?, ?)', [id, file[:filename], file_path, file[:type], uploaded_at])
+    # Response in JSON
+    { 
+        message: "File uploaded successfully!",
+        id: id,
+        file_name: file[:filename],
+        uploaded_at: uploaded_at
+    }.to_json
+end
+
+# Fetch FILE
+get '/upload/:id' do
+    id = params['id']
+    result = DB.execute("SELECT * FROM uploads WHERE id = ?", id).first
+    halt 404, { message: 'File not found' }.to_json unless result
+
+    # Send the file
+    send_file result['file_path'], file_name: result['file_name'], type: result['content_type']
+end
+
+# List all files in the FILES table
+get '/uploads' do
+    rows = DB.execute("SELECT * FROM uploads ORDER BY uploaded_at")
+    if rows.empty?
+        {message: "No objects stored."}.to_json
+    else
+        rows.map do |row|
+            {
+                id: row['id'],
+                file_name: row['file_name'],
+                created_at: row['uploaded_at']
+            }
+        end.to_json
+    end
+end
+
--- database/db_setup.rb
+++ database/db_setup.rb
@@ -5,6 +5,7 @@ DB_FILE = File.expand_path('../storage.db', __dir__)
 if File.exist?(DB_FILE)
   puts "Database already exists: #{DB_FILE}. Skipping setup."
 else
+  # Create a table for objects storage
   db = SQLite3::Database.new(DB_FILE)
   db.execute <<-SQL
     CREATE TABLE IF NOT EXISTS objects (
@@ -13,5 +14,18 @@ else
       created_at DATETIME
     );
   SQL
-  puts "Database initialized successfully: #{DB_FILE}"
+  puts "Object table initialized successfully: #{DB_FILE}"
+
+  # Create a table for file storage
+  db.execute <<~SQL
+      CREATE TABLE IF NOT EXISTS uploads (
+        id TEXT PRIMARY KEY,
+        file_name TEXT,
+        file_path TEXT,
+        content_type TEXT,
+        uploaded_at DATETIME
+      )
+  SQL
+  puts "Files table initialized successfully"
+  puts "Database initialized successfully"
 end
